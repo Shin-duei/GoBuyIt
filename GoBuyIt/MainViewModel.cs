85359baf053f78a0dd16ece73b32534e8905aec4
ShinDuei <shin.duei.chou@gmail.com>
2020-06-19 06:50:35 +0800

    header change

--- GoBuyIt/MainViewModel.cs
+++ GoBuyIt/MainViewModel.cs
@@ -4,6 +4,7 @@ using GoBuyIt.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Data.SQLite;
 using System.IO;
@@ -11,6 +12,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace GoBuyIt
 {
@@ -197,6 +200,75 @@ namespace GoBuyIt
                               }
 
                               var MemberInOrder = OrderList.FindAll(s => s.Membership == "V");
+                    }
+                    public ICommand ExecuteClickCommand
+                    {
+                              get { return new DelegateCommand(ImportOderClick, CanCommand); }
+                    }
+
+                    private void ImportOderClick()
+                    {
+                              //string OrderListPath = @"Order/order_2020_05_16_09_52_57.csv";
+                              // string MemberListPath = @"MemberList/方氏果乾會員.csv";
+                              string OrderListPath = @"C:\Users\Administrator\Downloads\order_2020_05_16_09_52_57.csv";
+                              string MemberListPath = @"C:\Users\Administrator\Downloads\方氏果乾會員.csv";
+                              FileProcessing.CsvTrans2Json<BaseTitle>(OrderListPath, out List<BaseTitle> _OrderViewList);
+                              FileProcessing.CsvTrans2Json<MemberListTitle>(MemberListPath, out List<MemberListTitle> MemberList);
+
+                              foreach (var Order in _OrderViewList)
+                              {
+                                        MemberList.ForEach(s =>
+                                        {
+                                                  if (Order.CustomerName == s.RealName && Order.CustomerEmail == s.Email)
+                                                            Order.Membership = "V";
+                                        });
+                              }
+
+                              var MemberInOrder = _OrderViewList.FindAll(s => s.Membership == "V");
+
+                              _OrderViewList.ForEach(s=> {
+                                        OrderViewList.Add(new OrderView(s));
+                              }
+                              );
+                    }
+
+                    private bool CanCommand()
+                    {
+                              return true;
+                    }
+          }
+
+          public static class CustomColumnHeadersProperty
+          {
+                    public static DependencyProperty ItemTypeProperty = DependencyProperty.RegisterAttached("ItemType", typeof(Type), typeof(CustomColumnHeadersProperty), new PropertyMetadata(OnItemTypeChanged));
+
+                    public static void SetItemType(DependencyObject obj, Type value)
+                    {
+                              obj.SetValue(ItemTypeProperty, value);
+                    }
+
+                    public static Type GetItemType(DependencyObject obj)
+                    {
+                              return (Type)obj.GetValue(ItemTypeProperty);
+                    }
+
+                    private static void OnItemTypeChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+                    {
+                              var dataGrid = sender as DataGrid;
+
+                              if (args.NewValue != null)
+                                        dataGrid.AutoGeneratingColumn += dataGrid_AutoGeneratingColumn;
+                              else
+                                        dataGrid.AutoGeneratingColumn -= dataGrid_AutoGeneratingColumn;
+                    }
+
+                    static void dataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+                    {
+                              var type = GetItemType(sender as DataGrid);
+
+                              var displayAttribute = type.GetProperty(e.PropertyName).GetCustomAttributes(typeof(DisplayAttribute), false).FirstOrDefault() as DisplayAttribute;
+                              if (displayAttribute != null)
+                                        e.Column.Header = displayAttribute.Name;
                     }
           }
 }
--- GoBuyIt/ViewModel/OrderView.cs
+++ GoBuyIt/ViewModel/OrderView.cs
@@ -1,7 +1,9 @@
 ﻿using GoBuyIt.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,14 +11,77 @@ namespace GoBuyIt.ViewModel
 {
           public class OrderView : ViewModelBase
           {
-                    private BaseTitle orderList;
+                    private BaseTitle OrderList;
 
-                    public BaseTitle OrderList
+                    public OrderView(BaseTitle orderList)
                     {
-                              get { return orderList; }
+                              OrderList = orderList;
+                              OwnerNumber = OrderList.OrderNumber;
+                              OwnerName = OrderList.OwnerName;
+                              OrderNumber = OrderList.OrderNumber;
+                              DateCreate = OrderList.DateCreate;
+                    }
+
+                    private string ownerNumber;
+                    /// <summary>
+                    /// 廠商編號
+                    /// </summary>
+                    [DataMember(Name = "廠商編號")]
+                    [JsonProperty(PropertyName = "廠商編號")]
+                    public string OwnerNumber
+                    {
+                              get { return ownerNumber; }
                               set
                               {
-                                        orderList = value;
+                                        ownerNumber = value;
+                                        OnPropertyChanged();
+                              }
+                    }
+
+                    private string ownerName;
+                    /// <summary>
+                    /// 廠商名稱
+                    /// </summary>
+                    [DataMember(Name = "廠商名稱")]
+                    [JsonProperty(PropertyName = "廠商名稱")]
+                    public string OwnerName
+                    {
+                              get { return ownerName; }
+                              set
+                              {
+                                        ownerName = value;
+                                        OnPropertyChanged();
+                              }
+                    }
+
+                    private string orderNumber;
+                    /// <summary>
+                    /// 訂單編號
+                    /// </summary>
+                    [DataMember(Name = "訂單編號")]
+                    [JsonProperty(PropertyName = "訂單編號")]
+                    public string OrderNumber
+                    {
+                              get { return orderNumber; }
+                              set
+                              {
+                                        orderNumber = value;
+                                        OnPropertyChanged();
+                              }
+                    }
+
+                    private string dateCreate;
+                    /// <summary>
+                    /// 建立日期
+                    /// </summary>
+                    [DataMember(Name = "建立日期")]
+                    [JsonProperty(PropertyName = "建立日期")]
+                    public string DateCreate
+                    {
+                              get { return dateCreate; }
+                              set
+                              {
+                                        dateCreate = value;
                                         OnPropertyChanged();
                               }
                     }
